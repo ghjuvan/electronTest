5003e0bf10b569431c518b1e993592116e348e67
ghjuvan <vaevconsult@gmail.com>
2016-04-07 00:41:13 +0200

    des trucs encore plus awesome

--- app/front/chat/chat.js
+++ app/front/chat/chat.js
@@ -6,19 +6,20 @@ const dialog = remote.dialog;
 
 angular
     .module('main', ['firebase'])
-    .controller('loginController', function ($scope, $firebaseArray) {
+    .controller('loginController', function ($scope, $firebaseArray, $http) {
 
         var ref = new Firebase('https://electronjs.firebaseio.com/rooms');
 
+         $http.get('../../../local/credential.json').success(function (user) {
+             $scope.user = user.password.email;
+         });
+
         $scope.rooms = $firebaseArray(ref);
         $scope.newRoom = '';
-
 
         $scope.createRoom = function (name) {
             $scope.rooms.$add({name: name}).then(function () {
                 dialog.showMessageBox({ message: `La room ${name} a bien été créée`, buttons: [`CIMER ALBERT`] });
             });
         };
-
-
     });
